6f8c0fbd98fb64dc100b614de39b9bbcc1255d13
salsarizkii <salsarizki.srs@gmail.com>
2025-01-28 23:34:07 +0700

    add blogFactory

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Blog extends Model
 {
- protected $fillable = ['title', 'slug', 'author', 'body'];
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'author', 'body'];
 };
 
---
+++ database/factories/blogFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\blog>
+ */
+class blogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'author' => $this->faker->name(),
+            'slug' => $this->faker->slug(),
+            'body' => $this->faker->paragraph(),
+        ];
+    }
+}
